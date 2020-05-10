03de057529b26c22b3e2fa384d477e4ab5b0af0f
Phong Nguyen <contact@phong.jp>
2020-05-10 23:19:47 +0900

    init commit

---
+++ lib_with_test/test/index.test.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import test from "../src"
+
+const localVue = new Vue();
+console.log('test vue path resolve: ', require.resolve('vue'));
+console.log('localVue instanceof Vue: ', localVue instanceof Vue)
+describe("Test", () => {
+  //  Inspect the raw component options
+  it("must be success", () => {
+    let testInstance = new test();
+    let result = testInstance.go(localVue);
+    expect(result).toBe(true);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+### Way to deal with conflict dependencies
+1. Use dedupe
+```
+$ npm dedupe
+```
+The above command will remove all duplicated dependencies in other package.
+For instance node_modules in src project will be removed
+
+[https://docs.npmjs.com/cli/dedupe]
